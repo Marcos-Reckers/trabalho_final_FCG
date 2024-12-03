913deab9974c26bc185944d38c5fe201c5908275
GuiDavilaP <guilhermedavilap@gmail.com>
2024-12-02 23:08:15 -0300

    objmodel
    Separei funções relacionadas ao modelo obj em novos arquivos .h e .cpp

---
+++ include/objmodel.h
@@ -0,0 +1,29 @@
+#ifndef OBJMODEL_H
+#define OBJMODEL_H
+
+#include <tiny_obj_loader.h>
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <cstdio> // Para printf e fprintf
+
+// Estrutura que representa um modelo geométrico carregado a partir de um
+// arquivo ".obj". Veja https://en.wikipedia.org/wiki/Wavefront_.obj_file .
+struct ObjModel
+{
+public:
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+
+    // Construtor que carrega o modelo
+    ObjModel(const char *filename, const char *basepath = nullptr, bool triangulate = true);
+};
+
+// Computa normais de um ObjModel, caso não existam.
+void ComputeNormals(ObjModel *model);
+
+// Função para debugging
+void PrintObjModelInfo(ObjModel *model);
+
+#endif // OBJMODEL_H
---
+++ src/objmodel.cpp
@@ -0,0 +1,120 @@
+#include "objmodel.h" // Inclui o header correspondente
+#include <iostream>      // Para std::cerr
+#include <tiny_obj_loader.h>
+#include <glm/vec4.hpp>
+#include "matrices.h"
+
+// Implementação do construtor de ObjModel
+ObjModel::ObjModel(const char *filename, const char *basepath, bool triangulate)
+{
+    printf("Carregando objetos do arquivo \"%s\"...\n", filename);
+
+    std::string fullpath(filename);
+    std::string dirname;
+
+    if (basepath == nullptr)
+    {
+        auto i = fullpath.find_last_of("/");
+        if (i != std::string::npos)
+        {
+            dirname = fullpath.substr(0, i + 1);
+            basepath = dirname.c_str();
+        }
+    }
+
+    std::string warn;
+    std::string err;
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, filename, basepath, triangulate);
+
+    if (!err.empty())
+        std::cerr << err << std::endl;
+
+    if (!ret)
+        throw std::runtime_error("Erro ao carregar modelo.");
+
+    for (const auto &shape : shapes)
+    {
+        if (shape.name.empty())
+        {
+            fprintf(stderr,
+                    "*********************************************\n"
+                    "Erro: Objeto sem nome dentro do arquivo '%s'.\n"
+                    "Veja https://www.inf.ufrgs.br/~eslgastal/fcg-faq-etc.html#Modelos-3D-no-formato-OBJ .\n"
+                    "*********************************************\n",
+                    filename);
+            throw std::runtime_error("Objeto sem nome.");
+        }
+        printf("- Objeto '%s'\n", shape.name.c_str());
+    }
+
+    printf("OK.\n");
+}
+
+// Função que computa as normais de um ObjModel, caso elas não tenham sido
+// especificadas dentro do arquivo ".obj"
+void ComputeNormals(ObjModel *model)
+{
+    if (!model->attrib.normals.empty())
+        return;
+
+    // Primeiro computamos as normais para todos os TRIÂNGULOS.
+    // Segundo, computamos as normais dos VÉRTICES através do método proposto
+    // por Gouraud, onde a normal de cada vértice vai ser a média das normais de
+    // todas as faces que compartilham este vértice.
+
+    size_t num_vertices = model->attrib.vertices.size() / 3;
+
+    std::vector<int> num_triangles_per_vertex(num_vertices, 0);
+    std::vector<glm::vec4> vertex_normals(num_vertices, glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
+
+    for (size_t shape = 0; shape < model->shapes.size(); ++shape)
+    {
+        size_t num_triangles = model->shapes[shape].mesh.num_face_vertices.size();
+
+        for (size_t triangle = 0; triangle < num_triangles; ++triangle)
+        {
+            assert(model->shapes[shape].mesh.num_face_vertices[triangle] == 3);
+
+            glm::vec4 vertices[3];
+            for (size_t vertex = 0; vertex < 3; ++vertex)
+            {
+                tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
+                const float vx = model->attrib.vertices[3 * idx.vertex_index + 0];
+                const float vy = model->attrib.vertices[3 * idx.vertex_index + 1];
+                const float vz = model->attrib.vertices[3 * idx.vertex_index + 2];
+                vertices[vertex] = glm::vec4(vx, vy, vz, 1.0);
+            }
+
+            const glm::vec4 a = vertices[0];
+            const glm::vec4 b = vertices[1];
+            const glm::vec4 c = vertices[2];
+
+            // PREENCHA AQUI o cálculo da normal de um triângulo cujos v��rtices
+            // estão nos pontos "a", "b", e "c", definidos no sentido anti-horário.
+            const glm::vec4 n = crossproduct((b - a), (c - a));
+
+            for (size_t vertex = 0; vertex < 3; ++vertex)
+            {
+                tinyobj::index_t idx = model->shapes[shape].mesh.indices[3 * triangle + vertex];
+                num_triangles_per_vertex[idx.vertex_index] += 1;
+                vertex_normals[idx.vertex_index] += n;
+                model->shapes[shape].mesh.indices[3 * triangle + vertex].normal_index = idx.vertex_index;
+            }
+        }
+    }
+
+    model->attrib.normals.resize(3 * num_vertices);
+
+    for (size_t i = 0; i < vertex_normals.size(); ++i)
+    {
+        glm::vec4 n = vertex_normals[i] / (float)num_triangles_per_vertex[i];
+        n /= norm(n);
+        model->attrib.normals[3 * i + 0] = n.x;
+        model->attrib.normals[3 * i + 1] = n.y;
+        model->attrib.normals[3 * i + 2] = n.z;
+    }
+}
+
+
+
+
